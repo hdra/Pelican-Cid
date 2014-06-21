7fc132046b2e2543ca1523ec27dc285a880ffab5
Hendra <hendra2392@gmail.com>
2014-06-21 19:05:24 +0800

    Setup gulp file for sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var prefix = require('gulp-autoprefixer');
 
 gulp.task('default', function(){
 
+    gulp.watch('src/sass/**/*.sass', function(event){
+        console.log( event.path+' was '+event.type+', recompiling sass files.');
+        gulp.src('src/sass/*.sass')
+                .pipe(sass({ style: 'compressed' }))
+                .pipe(gulp.dest('static/css'));
+        console.log('Compile done.');
+    });
 });
